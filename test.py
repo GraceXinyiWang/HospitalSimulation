cd737728184dbcc8c12942040ababcc81821af39
GraceXinyiWang <you@example.com>
2026-04-12 11:30:21 -0400

    organized

--- test.py
+++
@@ -1,5 +0,0 @@
-import os
-
-print("Current working directory:", os.getcwd())
-print("xlsx exists?", os.path.exists("df_selected.xlsx"))
-print("json exists?", os.path.exists("arrival_model_params.json"))
